91b721e9740a7f3619a74b952e73a59dba7c38ac
lt <1104623908@qq.com>
2019-02-19 10:42:03 +0800

    添加了一个主方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,7 +1,12 @@
 import org.junit.Test;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("saasdsdds");
+        System.out.println("saasdsdds");
+        System.out.println("saasdsdds");
+        System.out.println("saasdsdds");
+    }
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
